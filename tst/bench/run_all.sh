8bcbb80701e9bccf13c0d443b12e20b79982ddee
Michael Torpey <mct25@st-andrews.ac.uk>
2017-11-14 17:03:51 +0000

    Test 1

--- tst/bench/run_all.sh
+++ tst/bench/run_all.sh
@@ -2,7 +2,7 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd $SCRIPTPATH
 
-echo "Read(\"cong.bench.g\"); write_tests(5); quit;" | gap
+echo "Read(\"cong.bench.g\"); write_tests(5000); quit;" | gap
 echo "Read(\"cong.bench.g\"); do_benchmarks(); quit;" | gap
 echo "Read(\"cong.bench.g\"); do_gap_benchmarks(); quit;" | gap -r
 ./make_plots
